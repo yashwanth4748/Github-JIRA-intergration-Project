799c447eb54ffc700f8afee5aede324159fa85db
Yashwanth kumar Rokkam <123750192+yashwanth4748@users.noreply.github.com>
2024-02-16 20:47:41 +0530

    Update create-jira.py

--- create-jira.py
+++ create-jira.py
@@ -4,11 +4,11 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://veeramallaabhishek.atlassian.net/rest/api/3/issue"
+url = "https://yashwanth48.atlassian.net/rest/api/3/issue"
 
 API_TOKEN = ""
 
-auth = HTTPBasicAuth("", API_TOKEN)
+auth = HTTPBasicAuth("yashwanth4748@gmail.com", API_TOKEN)
 
 headers = {
   "Accept": "application/json",
